0fde1c122a648780dd44420e03ce6a5f25398de8
kavya1526 <201901474@daiict.ac.in>
2023-11-27 21:47:00 -0700

    Links between Circular Nodes

--- MC1/Network-Link-Graph/network-link-script.js
+++ MC1/Network-Link-Graph/network-link-script.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function () {
     ]).then(function (values) {
         const resumeData = processDataType(values[0]);
         const nodeGroups = groupByOrganization(resumeData);
-        const linksData = cleanLinksData(values[1]);
+        const linksData = values[1];
         console.log(resumeData);
         console.log(nodeGroups);
         console.log(linksData);
@@ -52,13 +52,12 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-
 function plotClusters(resumeData, nodeGroups, linksData) {
     console.log(linksData);
 
     const margin = { top: 50, right: 30, bottom: 120, left: 60 },
         width = 750 - margin.left - margin.right,
-        height = 750 - margin.top - margin.bottom;
+        height = 850 - margin.top - margin.bottom;
 
     const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
@@ -76,7 +75,7 @@ function plotClusters(resumeData, nodeGroups, linksData) {
     const getcirclePositionAttributes = (organization) => {
         const pentagonRadius = 280; // Adjust the radius as needed
         const angleOffset = Math.PI / 2; // Offset to start the pentagon from the top
-        const angleStep = (2 * Math.PI) / 5; // Angle between vertices
+        const angleStep = (2 * Math.PI) / 7; // Angle between vertices
 
         const positions = {
             'POKMember': { angle: 0 },
@@ -84,6 +83,8 @@ function plotClusters(resumeData, nodeGroups, linksData) {
             'GASTechBoard': { angle: 2 * angleStep },
             'GASTechEmployee': { angle: 3 * angleStep },
             'POKCommittee': { angle: 4 * angleStep },
+            'APA': { angle: 5 * angleStep },
+            'Government': { angle: 6 * angleStep },
         };
 
         return {
@@ -93,8 +94,10 @@ function plotClusters(resumeData, nodeGroups, linksData) {
         };
     };
 
+    
     // Create a force simulation for the smaller circles
     const simulation = d3.forceSimulation(resumeData)
+    simulation
         .force("x", d3.forceX().strength(0.05).x(d => {
             const organization = d.organization;
             const circlePosition = getcirclePositionAttributes(organization);
@@ -105,11 +108,71 @@ function plotClusters(resumeData, nodeGroups, linksData) {
             const circlePosition = getcirclePositionAttributes(organization);
             return circlePosition.cy; // Use the main circle's attributes directly
         }))
-        .force("collide", d3.forceCollide().radius(d => (d.impOfPeople / 9) * 20 + 9))
-        .on("tick", () => {
-            circles.attr("cx", d => d.x)
-                .attr("cy", d => d.y);
+        .force("collide", d3.forceCollide().radius(d => (d.impOfPeople / 9) * 20 + 9));
+        // .on("tick", () => {
+        //     circles.attr("cx", d => d.x)
+        //         .attr("cy", d => d.y);
+        // });
+
+    // simulation.start();
+    simulation.alpha(1).alphaMin(0.0001)
+    while (simulation.alpha() > simulation.alphaMin()) {
+        simulation.tick();
+    }
+    simulation.stop()
+
+    console.log(resumeData)
+
+    const links = svg.selectAll(".link")
+        .data(linksData.filter(d => parseFloat(d.frequency)>5))
+        .enter().append("line")  // You can use "line" instead of "path" if you want straight lines
+        .attr("class", "link")
+        .attr("stroke", "green")
+        .attr("stroke-width", 1)
+        .attr("x1", d => getXCoordinate(d.source))
+        .attr("y1", d => getYCoordinate(d.source))
+        .attr("x2", d => getXCoordinate(d.target))
+        .attr("y2", d => getYCoordinate(d.target))
+        .on("mouseover", function (event, d) {
+            // Highlight the hovered link and connected circles
+            d3.select(this).style("stroke", "green"); // Highlight the link
+    
+            circles.filter(node => node.name === d.source || node.name === d.target)
+                .transition()
+                .style("opacity", 1); // Highlight connected circles
+    
+            // Fade other elements
+            svg.selectAll(".link")
+                .filter(link => link !== d)
+                .transition()
+                .style("opacity", 0.1);
+    
+            circles.filter(node => node.name !== d.source && node.name !== d.target)
+                .transition()
+                .style("opacity", 0.1);
+    
+            // Show tooltip on mouseover
+            tooltip.html(`Source: ${d.source} <br>Target: ${d.target}`)
+                .style("opacity", 0.9)
+                .style("left", (event.pageX + 15) + "px")
+                .style("top", (event.pageY - 15) + "px");
+        })
+        .on("mouseout", function () {
+            // Restore styles and opacity on mouseout
+            d3.select(this).style("stroke", null); // Restore link style
+    
+            svg.selectAll(".link")
+                .transition()
+                .style("opacity", 1);
+    
+            circles.transition()
+                .style("opacity", 1);
+    
+            // Hide tooltip on mouseout
+            tooltip.html("")
+                .style("opacity", 0);
         });
+
 
     // Create smaller circles inside the main circles
     const circles = svg.selectAll(".node")
@@ -118,6 +181,8 @@ function plotClusters(resumeData, nodeGroups, linksData) {
         .attr("class", "node")
         .attr("stroke", "black")
         .attr("stroke-width", 2)
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y)
         .attr("fill", d => colorScale(d.organization))
         .attr("r", d => ((d.impOfPeople + 2) / 9) * 20)
         .on("mouseover", function (event, d) {
@@ -153,4 +218,26 @@ function plotClusters(resumeData, nodeGroups, linksData) {
             tooltip.html("")
                 .style("opacity", 0);
         });
+
+   console.log(typeof parseFloat(linksData[0].frequency))
+    
+    x = Object(resumeData[0])
+    console.log(resumeData[0], "object")
+    console.log(Object(resumeData[0]).x, "x value")
+    console.log(resumeData[0], "object")
+
+
+    function getXCoordinate(name) {
+        let node = resumeData.find(d => d.name == name);
+        node = Object(node)
+        return node ? node.x : 0;
+    }
+
+    function getYCoordinate(name) {
+        let node = resumeData.find(d => d.name == name);
+        node = Object(node)
+        return node ? node.y : 0;
+    }
+
+
 }
